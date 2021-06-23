8d4957acac308d559e36d4d7ea3032dd866fccaf
ketnoir <ketnoir4@gmail.com>
2021-06-23 20:16:56 +0300

    second commit

---
+++ homework1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Task 2
+
+
+# Output: You, Beetroot!
+print('Yo, Beetroot!')
+
+# Output: Make love not war!
+print('Make love', 'not war', sep=' ', end='!')
+
+# Output: Python is awesome
+print('Python', 'is awesome', sep=' ', end='')
+
+
+# Task 3
+
+# executing letter O
+print('#########\n', '#\t#\n'*3, '#########', sep='', end='\n\n')
+# executing letter H
+print('#\t#\n'*2, '#'*9, sep='', end='\n')
+print('#\t#\n'*2, end='\n')
+
---
+++ homework2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Task 1
+
+
+import datetime
+day = datetime.datetime.now()
+day = day.strftime('%A')
+name = 'Mykyta'
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+
+
+# Task 2
+
+
+first_name = 'Mykyta'
+last_name = 'Samchuk'
+print('Good evening,', first_name + ' ' + last_name + '!', 'Today is a great day to take a walk!')
+
+
+# Task 3
+
+
+a = 24
+b = 12
+
+# Addition
+print(a + b)
+
+# Subtraction
+print(a - b)
+
+# Division
+print(a / b)
+
+# Multiplication
+print(a * b)
+
+# Exponent
+print(a ** b)
+
+# Modulus
+print(a % b)
+
+# Floor division
+print(a // b)
+
+
+
+
+
